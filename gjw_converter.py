08139bc2f9f19a8715be47a1aa95ba9b7506df24
gjwo <grahamjwood@gmail.com>
2015-06-09 15:53:00 +0100

    Corrected warnings raised by Landscape largely unused imports

--- gjw_converter.py
+++ gjw_converter.py
@@ -1,18 +1,14 @@
 from __future__ import print_function, division
 import pandas as pd
 import numpy as np
-from copy import deepcopy
 import os
-from os.path import join, isdir, isfile
-from os import listdir
+from os.path import join, isfile
 import fnmatch
 import re
-from sys import stdout
 from nilmtk.utils import get_datastore
 from nilmtk.datastore import Key
-from nilmtk.timeframe import TimeFrame
 from nilmtk.measurement import LEVEL_NAMES
-from nilmtk.utils import get_module_directory, check_directory_exists
+from nilmtk.utils import check_directory_exists
 from nilm_metadata import convert_yaml_to_hdf5, save_yaml_to_datastore
 
 column_mapping = {
@@ -50,7 +46,7 @@ filename_suffix_mapping = {
 
 # DataFrame column names
 TIMESTAMP_COLUMN_NAME = "timestamp"
-ACTIVE_COLUMN_NAME = "active"
+ACTIVE_COLUMN_NAME = "VA"
 REACTIVE_COLUMN_NAME = "reactive"
 
 type_column_mapping = {
@@ -92,13 +88,12 @@ def convert_gjw(gjw_path, output_filename, format="HDF"):
             meter_devices.yaml
         other files    
     """
-    if gjw_path is None: gjwpath = home_dir
+    if gjw_path is None: gjw_path = home_dir
     check_directory_exists(gjw_path)
     os.chdir(gjw_path)
     gjw_path = os.getcwd()  # sort out potential issue with slashes or backslashes
     if output_filename is None:
         output_filename =join(home_dir,'HDF5','nilm_gjw_data.hdf5')
-    elec_path = join(gjw_path, 'building1','elec')
     # Open data store
     print( 'opening datastore', output_filename)
     store = get_datastore(output_filename, format, mode='w')
@@ -137,19 +132,19 @@ def convert_gjw(gjw_path, output_filename, format="HDF"):
     convert_yaml_to_hdf5(join(gjw_path, 'metadata'),output_filename)
     print("Done converting gjw to HDF5!")
 
-def _read_and_standardise_file(dir,ds,type):   
+def _read_and_standardise_file(cdir,ds,mtype):   
     """
     parameters 
-        dir  - the directory path where the files may be found
+        cdir  - the directory path where the files may be found
         ds   - the date string which identifies the pair of files
         type - the type of data to be read
     The filename is constructed using the appropriate prefixes and suffixes
     The data is then read, merged, de-duplicated, converted to the correct time zone
     and converted to a time series and resampled per second
     """
-    fn = filename_prefix_mapping[type]+ds+filename_suffix_mapping[type]+'.csv'
-    ffn = join(dir,fn)
-    df = pd.read_csv(ffn,names=[TIMESTAMP_COLUMN_NAME,type_column_mapping[type]])
+    fn = filename_prefix_mapping[mtype]+ds+filename_suffix_mapping[mtype]+'.csv'
+    ffn = join(cdir,fn)
+    df = pd.read_csv(ffn,names=[TIMESTAMP_COLUMN_NAME,type_column_mapping[mtype]])
     df.drop_duplicates(subset=[TIMESTAMP_COLUMN_NAME], inplace=True) # remove duplicate rows with same timestamp
     df.index = pd.to_datetime(df.timestamp.values, unit='s', utc=True) # convert the index to time based
     df = df.tz_convert(TIMEZONE) #deal with summertime etc. for London timezone
@@ -160,21 +155,21 @@ def _read_and_standardise_file(dir,ds,type):
     df.drop_duplicates(subset=TIMESTAMP_COLUMN_NAME, inplace=True)
     return df
 
-def _read_file_pair(dir,ds):
+def _read_file_pair(cdir,ds):
     """"
     parameters 
-        dir - the directory path where the files may be found
+        cdir - the directory path where the files may be found
         ds  - the date string which identifies the pair of files
     The files are processed individually then the columns merged on matching timestamps   
     """
-    df1 = _read_and_standardise_file(dir,ds,TYPE_A)
+    df1 = _read_and_standardise_file(cdir,ds,TYPE_A)
     #_summarise_dataframe(df1,'read file: '+TYPE_A)
-    df2 = _read_and_standardise_file(dir,ds,TYPE_R)
+    df2 = _read_and_standardise_file(cdir,ds,TYPE_R)
     #_summarise_dataframe(df2,'read file: '+TYPE_R)  
     df3 = pd.merge(df1,df2,on=TIMESTAMP_COLUMN_NAME, how='outer') #merge the two column types into 1 frame
     df3.fillna(value=0, inplace=True) # may need to enter initial entries to reactive sequence
     #_summarise_dataframe(df3,'return from merge and fillna)
-    print(df3[TIMESTAMP_COLUMN_NAME].head(1).format(),"to",df3[TIMESTAMP_COLUMN_NAME].tail(1).format()) #print first and last entries
+    print(df3[TIMESTAMP_COLUMN_NAME].head(1),"to",df3[TIMESTAMP_COLUMN_NAME].tail(1)) #print first and last entries
     return df3
 
 def _prepare_data_for_toolkit(df):
