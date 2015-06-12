61793f6ddd4a7344fc2300d22854a87ad46f63ab
gjwo <grahamjwood@gmail.com>
2015-06-12 21:47:29 +0100

    rerun with updated metadata and changes to refresh module

--- gjw_refresh_metadata.py
+++ gjw_refresh_metadata.py
@@ -1,11 +1,8 @@
 from __future__ import print_function, division
-from copy import deepcopy
 import os
-from os.path import join, isdir, isfile
-from os import listdir
-from sys import stdout
+from os.path import join
 from nilmtk.utils import check_directory_exists
-from nilm_metadata import convert_yaml_to_hdf5, save_yaml_to_datastore
+from nilm_metadata import convert_yaml_to_hdf5
 
 home_dir='/Users/GJWood/nilm_gjw_data' # path to input data
 
@@ -34,7 +31,7 @@ def refresh_gjw_metadata(gjw_path, output_filename, format="HDF"):
             meter_devices.yaml
         other files    
     """
-    if gjw_path is None: gjwpath = home_dir
+    if gjw_path is None: gjw_path = home_dir
     check_directory_exists(gjw_path)
     os.chdir(gjw_path)
     gjw_path = os.getcwd()  # sort out potential issue with slashes or backslashes
